3869df50d6a39442ceb7f20783a9b43a61b4753e
Alberto <albertovinicius3@gmail.com>
2023-01-17 16:42:21 -0300

    remove unused code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,12 @@
-import { FilterProvider } from '@contexts/FilterContext';
 import * as RTooltip from '@radix-ui/react-tooltip';
 import '@styles/globals.css';
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <FilterProvider>
-            <RTooltip.Provider delayDuration={25}>
-                <Component {...pageProps} />
-            </RTooltip.Provider>
-        </FilterProvider>
+        <RTooltip.Provider delayDuration={50}>
+            <Component {...pageProps} />
+        </RTooltip.Provider>
     );
 }
 

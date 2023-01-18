0c0e42cc58104df4a01ef71ea820400994b7a10a
Alberto <albertovinicius3@gmail.com>
2023-01-18 14:33:19 -0300

    feat: Logo Brand Banner

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -15,7 +15,7 @@ export default function Document() {
                     rel="stylesheet"
                 />
             </Head>
-            <body>
+            <body className="bg-blue-700">
                 <Main />
                 <NextScript />
             </body>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,41 @@
-import type { NextPage } from "next";
-import Head from "next/head";
+import GitHubProfile from '@components/GitHubProfile';
+import IntroBackgroundImage from '@imgs/intro-background.png';
+import LogoAlbertoDeveloperImage from '@imgs/logo-albertodeveloper.svg';
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
 
 const Home: NextPage = () => {
     return (
-        <div className="flex min-h-screen flex-col items-center justify-center py-2">
+        <div className="">
             <Head>
-                <title>Home</title>
+                <title>
+                    Blog do Alberto | Onde você experimenta minha mente
+                </title>
             </Head>
 
-            <main>
-                Tela inicial
+            <Image
+                src={IntroBackgroundImage}
+                alt="Banner Blog do Alberto"
+                className="w-full h-[18.5rem] object-cover absolute -z-10 top-0 left-0 right-0"
+                width={1440}
+                height={296}
+            />
+
+            <main className="mx-auto w-full max-w-4xl px-6">
+                {/* Logo Brand Container */}
+                <div className="flex flex-col gap-1 items-center justify-center mt-14 mb-10">
+                    <Image
+                        src={LogoAlbertoDeveloperImage}
+                        alt="Logotipo Website albertodeveloper.com"
+                    />
+
+                    <h1 className="font-titillium font-bold text-blue-50 text-4xl">
+                        Blog do Alberto
+                    </h1>
+                </div>
+                {/* Card GitHub Profile */}
+                <GitHubProfile />
             </main>
         </div>
     );

e5ddba86e0c1494b58fa6f2d39faea0d897aa3cf
Alberto <albertovinicius3@gmail.com>
2023-01-19 14:20:23 -0300

    feat: total posts

---
+++ src/components/TotalPosts/index.tsx
@@ -0,0 +1,17 @@
+import usePosts from '@hooks/usePosts';
+import { FunctionComponent } from 'react';
+
+/** 
+ * Renderiza o total de posts/publicações (GitHub Issues), formatado em texto
+ */
+const TotalPosts: FunctionComponent = () => {
+    const { totalPosts } = usePosts();
+
+    return (
+        <p className="text-[#7B96B2] text-lg">
+            <span>{totalPosts}</span> publicações
+        </p>
+    );
+};
+
+export default TotalPosts;

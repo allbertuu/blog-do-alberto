57ded130b0fbccf53f64557bced04584e4fb2ba9
Alberto Santos <89992304+allbertuu@users.noreply.github.com>
2023-02-19 21:39:51 -0300

    fix: remove a bit of lateral padding
    to better visibility and legibility

--- src/components/PostBody/index.tsx
+++ src/components/PostBody/index.tsx
@@ -14,7 +14,7 @@ const PostBody: FunctionComponent<PostBodyProps> = ({ postData }) => {
         '[&_h2]:font-bold [&_h2]:text-2xl [&_h2]:mb-2 [&_h2]:mt-6 lg:[&_h2]:mt-8 [&_h3]:font-bold [&_h3]:text-lg [&_h3]:mt-4 [&_p]:mb-2 [&_a]:text-red-500 [&_a]:underline [&_blockquote]:before:absolute [&_blockquote]:before:bg-red-500 [&_blockquote]:before:w-1 [&_blockquote>p]:before:mr-3 [&_blockquote]:before:h-full [&_blockquote]:relative [&_blockquote]:italic [&_blockquote]:mb-8 [&_blockquote]:text-blue-100';
 
     return (
-        <div className={classNames('py-8 px-2 sm:px-10', 'text-blue-50')}>
+        <div className={classNames('py-8 px-1 sm:px-10', 'text-blue-50')}>
             {postData ? (
                 <ReactMarkdown className={REACT_MARKDOWN_STYLES}>
                     {body || DEFAULT_BODY}

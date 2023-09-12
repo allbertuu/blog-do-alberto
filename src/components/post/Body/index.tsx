db3abfaae0cf8e46425d5dc46ecbb66c4ac80711
Alberto <albertovinicius3@gmail.com>
2023-09-11 23:25:13 -0300

    feat: format post body text

--- src/components/post/Body/index.tsx
+++ src/components/post/Body/index.tsx
@@ -11,10 +11,10 @@ const PostBody: FunctionComponent<PostBodyProps> = ({ postData }) => {
     const { body } = postData || {};
     const DEFAULT_BODY = '### Sem dados para exibir.';
     const REACT_MARKDOWN_STYLES =
-        '[&_h2]:font-bold [&_h2]:text-2xl [&_h2]:mb-2 [&_h2]:mt-6 lg:[&_h2]:mt-8 [&_h3]:font-bold [&_h3]:text-lg [&_h3]:mt-4 [&_p]:mb-2 [&_a]:text-red-500 [&_a]:underline [&_blockquote]:before:absolute [&_blockquote]:before:bg-red-500 [&_blockquote]:before:w-1 [&_blockquote>p]:before:mr-3 [&_blockquote]:before:h-full [&_blockquote]:relative [&_blockquote]:italic [&_blockquote]:mb-8 [&_blockquote]:text-blue-100';
+        '[&_h2]:font-bold [&_h2]:text-2xl [&_h2]:mb-2 [&_h2]:mt-6 lg:[&_h2]:mt-8 [&_h3]:font-bold [&_h3]:text-lg [&_h3]:mt-4 [&_p]:mb-2 [&_a]:text-red-500 [&_a]:underline [&_blockquote]:before:absolute [&_blockquote]:before:bg-red-500 [&_blockquote]:before:w-1 [&_blockquote>p]:before:mr-3 [&_blockquote]:before:h-full [&_blockquote]:relative [&_blockquote]:italic [&_blockquote]:text-blue-100';
 
     return (
-        <div className={classNames('py-8 sm:px-10', 'text-blue-50')}>
+        <div className={classNames('py-8 sm:px-10')}>
             {postData ? (
                 <ReactMarkdown className={REACT_MARKDOWN_STYLES}>
                     {body || DEFAULT_BODY}

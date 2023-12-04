c5625c270140b8849f610820c437e1f7a6178f52
Alberto <albertovinicius3@gmail.com>
2023-12-04 13:00:51 -0300

    chore: add toast config container

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,31 @@ import { PostsProvider } from '@contexts/PostsContext';
 import * as RTooltip from '@radix-ui/react-tooltip';
 import '@styles/globals.css';
 import type { AppProps } from 'next/app';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <PostsProvider>
-            <RTooltip.Provider delayDuration={50}>
-                <Component {...pageProps} />
-            </RTooltip.Provider>
-        </PostsProvider>
+        <>
+            <ToastContainer
+                position="top-left"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
+
+            <PostsProvider>
+                <RTooltip.Provider delayDuration={50}>
+                    <Component {...pageProps} />
+                </RTooltip.Provider>
+            </PostsProvider>
+        </>
     );
 }
 

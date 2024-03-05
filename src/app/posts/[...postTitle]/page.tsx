b1670dca55fea1788e25036703c69d4de72ed706
Alberto <albertovinicius3@gmail.com>
2024-03-05 15:43:18 -0300

    feat(page): add specific metadata as `title` and `description` for the post page

--- src/app/posts/[...postTitle]/page.tsx
+++ src/app/posts/[...postTitle]/page.tsx
@@ -1,6 +1,23 @@
 import { BlogBanner, LogoBrandContainer } from '@/components';
 import { PostBody } from '@/components/PostBody';
 import { PostHeader } from '@/components/PostHeader';
+import { fetchIssue } from '@/services/github.api';
+import { Metadata } from 'next';
+
+type Props = {
+  params: { postTitle: string[] };
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const issueNumber = params.postTitle[1];
+
+  const issue = await fetchIssue(issueNumber);
+
+  return {
+    title: `${issue.title.slice(0, 50)}...`,
+    description: `${issue.body.slice(0, 160)}...`,
+  };
+}
 
 export default function Page() {
   return (
